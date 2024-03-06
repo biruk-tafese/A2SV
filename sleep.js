fea1312fba2a0e0029931dbb8e3403e77d69facf
Biruk Tafesse <98220455+biruk-tafese@users.noreply.github.com>
2024-03-06 15:18:02 +0300

    promise resolved based on time

---
+++ sleep.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} millis
+ * @return {Promise}
+ */
+async function sleep(millis) {
+   
+    return new Promise((resolved)=> {
+        setTimeout(resolved, millis);
+    })
+}
+
+/** 
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
