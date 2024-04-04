55f37fc0c07678e1f84b1ba9ddab403b8b53611f
Biruk Tafesse <98220455+biruk-tafese@users.noreply.github.com>
2024-04-04 21:00:09 +0300

    maximum depth of a character

---
+++ maxDepth.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var maxDepth = function(s) {
+    let maxDepth = 0;
+    let currentDepth = 0;
+
+    for(let char of s) {
+         if(char === '(') {
+            currentDepth++;
+            maxDepth = Math.max(maxDepth, currentDepth);
+         } else if(char === ')') {
+            currentDepth--;
+         }
+    }
+
+    return maxDepth;
+};
